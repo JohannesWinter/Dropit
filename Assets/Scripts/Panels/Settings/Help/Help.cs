ea8a27ced3b3110ff54a67d3e4e9974983fddb2c
JohannesWinter <jvf.winter@web.de>
2025-11-19 21:19:09 +0100

    AddedScrollableHelpMenu

--- Assets/Scripts/Panels/Settings/Help/Help.cs
+++ Assets/Scripts/Panels/Settings/Help/Help.cs
@@ -10,15 +10,17 @@ public class Help : MonoBehaviour
     public Button[] buttons;
     public GameObject[] pages;
     public RectTransform buttonBar;
-    Vector3 barStart;
+    public Scrollbar scrollbar;
+    public float buttonDistance;
+    public float visibleButtons;
     // Start is called before the first frame update
     void Start()
     {
         for (int i = 0; i < buttons.Length; i++)
         {
-            buttons[i].onClick.AddListener(() => { SetHelpPage(i); });
+            int index = i;
+            buttons[i].onClick.AddListener(() => { SetHelpPage(index); });
         }
-        barStart = buttonBar.localPosition + new Vector3(buttonBar.localScale.x * 0.5f, 0, 0);
     }
 
     // Update is called once per frame
@@ -43,7 +45,7 @@ public class Help : MonoBehaviour
         {
             overlay.SetActive(false);
         }
-        SetButtonPositionsInBar();
+        UpdateButtonSizesAndScale();
     }
 
     void SetHelpPage(int page)
@@ -55,7 +57,37 @@ public class Help : MonoBehaviour
     {
         for (int i = 0; i < buttons.Length; i++)
         {
-            buttons[i].gameObject.GetComponent<RectTransform>().localPosition = barStart - new Vector3(i * 150f, 0, 0);
+        }
+    }
+    void UpdateButtonSizesAndScale()
+    {
+        for (int i = 0; i < buttons.Length; i++)
+        {
+            //position
+            Vector3 startPosition = Vector3.zero - new Vector3(0, i * buttonDistance, 0);
+            Vector3 actualPosition = startPosition + new Vector3(0, Mathf.Max(0, buttonDistance * ((buttons.Length - 1 - visibleButtons) * scrollbar.value)), 0);
+
+            Button currentButton = buttons[i];
+            RectTransform currentButtonTransfrom = currentButton.GetComponent<RectTransform>();
+            currentButtonTransfrom.localPosition = actualPosition;
+
+            //scale
+            float buttonHeight;
+            if (currentButtonTransfrom.localPosition.y <= 0 && currentButtonTransfrom.localPosition.y >= -buttonDistance * visibleButtons)
+            {
+                buttonHeight = 1;
+            }
+            else if (currentButtonTransfrom.localPosition.y > 0)
+            {
+                buttonHeight = Mathf.Max(0, 1 - currentButtonTransfrom.localPosition.y / buttonDistance);
+                currentButtonTransfrom.Translate(0, -(1 - buttonHeight) * currentButtonTransfrom.rect.height * 0.5f, 0, Space.Self);
+            }
+            else
+            {
+                buttonHeight = Mathf.Max(0, 1 - Mathf.Abs(currentButtonTransfrom.localPosition.y + visibleButtons * buttonDistance) / buttonDistance);
+                currentButtonTransfrom.Translate(0, +(1 - buttonHeight) * currentButtonTransfrom.rect.height * 0.5f, 0, Space.Self);
+            }
+            currentButtonTransfrom.localScale = new Vector3(currentButtonTransfrom.localScale.x, buttonHeight, currentButtonTransfrom.localScale.z);
         }
     }
 }
