e3620f6c75055b8c1d4f0a74942dd39c41e8b8cc
JohannesWinter <jvf.winter@web.de>
2025-12-07 23:12:35 +0100

    StartedCommunicationMassage

--- Assets/Scripts/Story/Communication/CommunicationManager.cs
+++ Assets/Scripts/Story/Communication/CommunicationManager.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class CommunicationManager : MonoBehaviour
 {
+    bool inCommunication = false;
     // Start is called before the first frame update
     void Start()
     {
