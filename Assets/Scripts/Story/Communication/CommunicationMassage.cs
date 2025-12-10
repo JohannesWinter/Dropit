a8eeff846a99e85f92322755ca877e9c824082c5
JohannesWinter <jvf.winter@web.de>
2025-12-10 22:48:56 +0100

    AddedNoiseLineFolder

--- Assets/Scripts/Story/Communication/CommunicationMassage.cs
+++ Assets/Scripts/Story/Communication/CommunicationMassage.cs
@@ -15,6 +15,8 @@ public class CommunicationMassage : MonoBehaviour
     public GameObject overlay;
     public RectTransform personProfilePosition;
     public NoiseVolumeUI noiseVolumeUI;
+    public Transform noiseLinesFolder;
+    public GameObject[] noiseLines;
 
     // Update is called once per frame
 
@@ -25,6 +27,11 @@ public class CommunicationMassage : MonoBehaviour
         this.gameObject.GetComponent<RectTransform>().localPosition = Vector3.zero;
         personName.text = personProfile.gameObject.name; // gameobject has to have name of person
         noiseVolumeUI.trackSound = messageSound;
+        noiseLines = new GameObject[noiseLinesFolder.childCount];
+        for (int i = 0; i < noiseLinesFolder.childCount; i++)
+        {
+            noiseLines[i] = noiseLinesFolder.GetChild(i).gameObject;
+        }
         overlay.SetActive(true);
     }
     void Update()
