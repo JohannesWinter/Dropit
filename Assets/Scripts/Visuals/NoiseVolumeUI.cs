db5ef8f84e0e20436a90bb54a8e31a061ed95fef
JohannesWinter <jvf.winter@web.de>
2025-12-10 23:05:14 +0100

    SetNoiseAmplituesGenerator

--- Assets/Scripts/Visuals/NoiseVolumeUI.cs
+++ Assets/Scripts/Visuals/NoiseVolumeUI.cs
@@ -7,12 +7,21 @@ public class NoiseVolumeUI : MonoBehaviour
     public PlaySound trackSound;
     public Transform noiseLinesFolder;
     GameObject[] noiseLines;
+    public float[] noiseAmplitudes;
     void Start()
     {
         noiseLines = new GameObject[noiseLinesFolder.childCount];
         for (int i = 0; i < noiseLinesFolder.childCount; i++)
         {
             noiseLines[i] = noiseLinesFolder.GetChild(i).gameObject;
+        }
+        noiseAmplitudes = new float[noiseLines.Length];
+        int x = noiseAmplitudes.Length / 2;
+        for (int i = noiseAmplitudes.Length / 2; i >= 0; i--)
+        {
+            noiseAmplitudes[i] = ((float)i / 1.2f + 0.17f) / (noiseAmplitudes.Length / 2);
+            noiseAmplitudes[x] = ((float)i / 1.2f + 0.17f) / (noiseAmplitudes.Length / 2);
+            x++;
         }
     }
     void Update()
